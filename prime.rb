0b4e99ef7076f1b5711a718f3bd4153643231c7b
Anthony Lam <al3101@nyu.edu>
2018-05-06 04:00:36 +0000

    automatically backed up by learn

--- prime.rb
+++ prime.rb
@@ -1,13 +1,9 @@
 # Add  code here!
 def prime?(number)
-  if number <= 1
+  if number <= 1 || number % 2 == 0 
     return false
-  if number % 2 == 0 
-    return false 
   end 
   
-  
-    
   for num in (1..limit).to_a
     if number % num == 0
       return false 
