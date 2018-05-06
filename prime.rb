010bab71059daf34f3cddc07d02d50e9c444db6f
Anthony Lam <al3101@nyu.edu>
2018-05-06 03:30:09 +0000

    automatically backed up by learn

--- prime.rb
+++ prime.rb
@@ -1,12 +1,6 @@
 # Add  code here!
 def prime(number)
   limit = 1 + (number/2)
-  
-  if number <= 1 
-    return false
-  elsif number < 4
-    return true 
-  end 
     
   for num in (1..limit)
     if number % num == 0
